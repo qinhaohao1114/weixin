0e73db60bbddfa9a3395e8abae2a342eaba46fc5
qinhaohao <hhqin@dfocuspace.com>
2019-07-18 17:27:47 +0800

    提交代码

--- src/main/java/com/weixin/publicnation/business/SubscribeBusiness.java
+++ src/main/java/com/weixin/publicnation/business/SubscribeBusiness.java
@@ -4,19 +4,29 @@ import com.weixin.publicnation.annotation.BusinessContent;
 import com.weixin.publicnation.annotation.BusinessHandle;
 import com.weixin.publicnation.constant.MsgType;
 import com.weixin.publicnation.utils.TextMessageUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
 @BusinessHandle(type = MsgType.EVENT)
+@Slf4j
 public class SubscribeBusiness {
 
 
     @BusinessContent(content = "subscribe")
     public String subscribe(Map<String,String> params,String fromUser,String toUser){
 
+        log.info("获得新关注");
+
         return TextMessageUtil.initMessage(fromUser,toUser);
     }
+    @BusinessContent(content = "unsubscribe")
+    public String unsubscribe(Map<String,String> params,String fromUser,String toUser){
 
+        log.info("取消关注");
+
+        return TextMessageUtil.initMessage(fromUser,toUser);
+    }
 }
